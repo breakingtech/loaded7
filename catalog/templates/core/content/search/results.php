24b275755ca49ded0b506f2dbde333e6b0a1536b
gulsarrays <gulmohar@loadedcommerce.com>
2014-05-27 19:14:23 +0530

    B3275 catalog search listing pagination not working

--- catalog/templates/core/content/search/results.php
+++ catalog/templates/core/content/search/results.php
@@ -14,12 +14,48 @@
   <div class="col-sm-12 col-lg-12">
     <h1 class="no-margin-top"><?php echo $lC_Template->getPageTitle(); ?></h1> 
     <?php 
+    /*if (PRODUCT_LIST_FILTER == '1') echo lC_Template_output::getManufacturerFilter();
+    $Qlisting = lC_Template_output::getProductsListingSql();*/
+
+    if ( ($Qlisting->numberOfRows() > 0) && ( (PREV_NEXT_BAR_LOCATION == '1') || (PREV_NEXT_BAR_LOCATION == '3') ) ) {
+      ?>
+      <!-- PAGINATION-->
+      <div class="product-listing-module-pagination margin-bottom">
+        <div class="pull-left large-margin-bottom"><?php echo $Qlisting->getBatchTotalPages($lC_Language->get('result_set_number_of_products')); ?></div>
+        <div class="pull-right large-margin-bottom no-margin-top">
+          <ul class="pagination no-margin-top no-margin-bottom">
+            <?php echo $Qlisting->getBatchPageLinks('page', lc_get_all_get_params(array('page', 'info', 'x', 'y')), false); ?>
+          </ul>
+        </div>
+      </div><div class="clear-both"></div>
+      <!-- /PAGINATION-->   
+      <?php
+    }
+    ?>
+    <?php 
     if (file_exists(DIR_FS_TEMPLATE . 'modules/product_listing.php')) {
       require($lC_Vqmod->modCheck(DIR_FS_TEMPLATE . 'modules/product_listing.php'));
     } else {
       require($lC_Vqmod->modCheck('includes/modules/product_listing.php')); 
     }
-    ?>   
+    ?> 
+    </div><div class="clear-both"></div>
+    <?php
+    if ( ($Qlisting->numberOfRows() > 0) && ((PREV_NEXT_BAR_LOCATION == '2') || (PREV_NEXT_BAR_LOCATION == '3')) ) {
+      ?>
+      <!-- PAGINATION-->
+      <div class="product-listing-module-pagination">
+        <div class="pull-left"><?php echo $Qlisting->getBatchTotalPages($lC_Language->get('result_set_number_of_products')); ?></div>
+        <div class="pull-right no-margin-bottom no-margin-top">
+          <ul class="pagination no-margin-top no-margin-bottom">
+            <?php echo $Qlisting->getBatchPageLinks('page', lc_get_all_get_params(array('page', 'info', 'x', 'y')), false); ?>
+          </ul>
+        </div>
+      </div><div class="clear-both"></div>
+      <!-- /PAGINATION--> 
+      <?php 
+    }
+    ?> 
   </div>
 </div>     
 <div class="button-set clearfix large-margin-bottom">
